74c9a24f2db264950a9ae51db05f073f46a79629
Justyna Dolińska <secansoida@users.noreply.github.com>
2016-04-23 16:19:19 +0200

    Added Mantle

---
+++ Podfile
@@ -0,0 +1,5 @@
+inhibit_all_warnings!
+
+target 'SimpleTumblrClient' do
+  pod 'Mantle'
+end
---
+++ Pods/Target Support Files/Pods-SimpleTumblrClient/Pods-SimpleTumblrClient-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleTumblrClient : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleTumblrClient
+@end
