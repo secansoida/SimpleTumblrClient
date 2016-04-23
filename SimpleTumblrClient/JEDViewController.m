73d67d48d81bed2b0a4e6ac6a9739c276f5d047c
Justyna Dolińska <secansoida@users.noreply.github.com>
2016-04-23 16:12:04 +0200

    Simple request

--- SimpleTumblrClient/JEDViewController.m
+++ SimpleTumblrClient/JEDViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "JEDViewController.h"
+@import JavaScriptCore;
 
 @interface JEDViewController ()
 
@@ -18,10 +19,34 @@
     [super viewDidLoad];
     UILabel *helloLabel = [UILabel new];
     helloLabel.text = @"Hello world!";
-    [helloLabel sizeToFit];
-    helloLabel.center = self.view.center;
+    helloLabel.frame = self.view.bounds;
     helloLabel.textColor = [UIColor blueColor];
+    helloLabel.numberOfLines = 0;
     [self.view addSubview:helloLabel];
+
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+    NSURL *url = [NSURL URLWithString:@"https://pixeloutput.tumblr.com/api/read/json"];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+
+    void (^completion)(NSData *, NSURLResponse *, NSError *) = ^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSString *text;
+        if (data) {
+            text = [[NSString alloc] initWithData:data encoding:4];
+            JSContext *context = [JSContext new];
+            [context evaluateScript:text];
+            NSLog(@"%@", [context[@"tumblr_api_read"] toDictionary]);
+        } else {
+            text = [NSString stringWithFormat:@"%@", error];
+        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+            helloLabel.text = text;
+        });
+    };
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:url
+                                            completionHandler:completion];
+    [dataTask resume];
+    
 }
 
 - (void)didReceiveMemoryWarning {
