7599865ce393a3259992c6c84c5aa4d77ba638d8
Justyna Dolińska <secansoida@users.noreply.github.com>
2016-04-25 19:05:57 +0200

    Added search bar

--- SimpleTumblrClient/JEDViewController.m
+++ SimpleTumblrClient/JEDViewController.m
@@ -11,7 +11,7 @@
 
 @import JavaScriptCore;
 
-@interface JEDViewController ()
+@interface JEDViewController () <UISearchBarDelegate>
 
 @property (nonatomic, strong) JEDFeedFetcher *feedFetcher;
 
@@ -22,12 +22,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    UILabel *helloLabel = [UILabel new];
-    helloLabel.text = @"Hello world!";
-    helloLabel.frame = self.view.bounds;
-    helloLabel.textColor = [UIColor blueColor];
-    helloLabel.numberOfLines = 0;
-    [self.view addSubview:helloLabel];
+    [self setupSearchBar];
 
     NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
@@ -36,6 +31,43 @@
 
     NSString *username = @"pixeloutput";
 
+    [self showFeedForUsername:username];
+    
+}
+
+- (void)setupSearchBar
+{
+    UISearchBar *searchBarAppearance = [UISearchBar appearance];
+    [searchBarAppearance setBarTintColor:[UIColor whiteColor]];
+    [searchBarAppearance setTintColor:[UIColor greenColor]];
+    UITextField *textFieldAppearance = [UITextField appearanceWhenContainedInInstancesOfClasses:@[[UISearchBar class]]];
+    UIFont *font = [UIFont fontWithName:@"Courier New" size:14] ?: [UIFont systemFontOfSize:14];
+    [textFieldAppearance setDefaultTextAttributes:@{
+                                                    NSFontAttributeName : font,
+                                                    NSForegroundColorAttributeName : [UIColor greenColor],
+                                                    }];
+    [textFieldAppearance setBackgroundColor:[UIColor blackColor]];
+    [textFieldAppearance setBorderStyle:UITextBorderStyleNone];
+
+    UISearchBar *searchBar = [UISearchBar new];
+
+    searchBar.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    searchBar.frame = CGRectMake(0,
+                                 CGRectGetHeight([UIApplication sharedApplication].statusBarFrame),
+                                 CGRectGetWidth(self.view.bounds),
+                                 32);
+    searchBar.delegate = self;
+    searchBar.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleBottomMargin;
+    [self.view addSubview:searchBar];
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+
+- (void)showFeedForUsername:(NSString *)username
+{
     [self.feedFetcher fetchFeedForUsername:username withCompletion:^(JEDFeedResponse *response, NSError *error) {
         if (response) {
             NSLog(@"%@", response);
@@ -43,12 +75,13 @@
             NSLog(@"%@", error);
         }
     }];
-    
 }
 
-- (UIStatusBarStyle)preferredStatusBarStyle
+#pragma mark - UISearchBarDelegate
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
 {
-    return UIStatusBarStyleLightContent;
+    [self showFeedForUsername:searchBar.text];
 }
 
 @end
